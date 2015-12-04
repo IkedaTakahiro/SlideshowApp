39e62e104e2d465e280f29407a32a897069cb462
池田享浩 <ikedatakahiro@ikedatooruhiroshi-no-MacBook-Air.local>
2015-12-05 00:06:17 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideshowApp
+//
+//  Created by 池田享浩 on 2015/12/05.
+//  Copyright (c) 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SlideshowApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SlideshowApp
+//
+//  Created by 池田享浩 on 2015/12/05.
+//  Copyright (c) 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
