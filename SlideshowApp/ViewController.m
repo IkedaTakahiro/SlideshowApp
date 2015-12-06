94c351d8d29a3f87f2347f65baac985520e7beab
池田享浩 <ikedatakahiro@rrcs-172-254-99-25.nyc.biz.rr.com>
2015-12-06 21:22:12 +0900

    test

--- SlideshowApp/ViewController.m
+++ SlideshowApp/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController (){
+ UIImageView *imageView;
+ NSInteger count;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +19,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    count = 0;
+    [self background];
+    [self nextButton];
+    [self backButton];
+}
+
+-(void)background{
+    imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,320,568)];
+    imageView.image = [UIImage imageNamed:@"gazou0.png"];
+    [self.view addSubview:imageView];
+}
+
+-(void)nextButton{
+    UIButton *nbutton = [UIButton buttonWithType:UIButtonTypeCustom];//defaultのボタン
+    [nbutton setImage:[UIImage imageNamed:@"button3.png"] forState:UIControlStateNormal];
+    nbutton.frame = CGRectMake(0,0,50,20);
+    nbutton.center = CGPointMake(240,50);
+    //[nbutton setTitle:@"→" forState:UIControlStateNormal];
+    [nbutton addTarget:self action:@selector(nextSlide:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:nbutton];
+}
+
+-(void)backButton{
+    UIButton *bbutton = [UIButton buttonWithType:UIButtonTypeCustom];//defaultのボタン
+    [bbutton setImage:[UIImage imageNamed:@"button4.png"] forState:UIControlStateNormal];
+    bbutton.frame = CGRectMake(0,0,50,20);
+    bbutton.center = CGPointMake(80,50);
+    //[bbutton setTitle:@"←" forState:UIControlStateNormal];
+    [bbutton addTarget:self action:@selector(backSlide:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:bbutton];
+}
+
+
+-(void)nextSlide:(id)sender{
+    count++;
+    if (count > 5) {
+        count = 0;
+    }
+    imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"gazou%ld.png",(long)count]];
+}
+
+-(void)backSlide:(id)sender{
+    count--;
+    if ( 0 <= count  && count <= 4){
+        imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"gazou%ld.png",(long)count]];
+    } else if(count == -1){
+        count = 5;
+        imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"gazou%ld.png",(long)count]];
+
+    }
 }
 
 - (void)didReceiveMemoryWarning {
